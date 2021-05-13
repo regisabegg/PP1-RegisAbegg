6f91c02ac0a621ce06a84846dbf51ac26f619340
Regis Abegg <regisabegg@gmail.com>
2021-05-12 21:38:00 -0300

    Inciando o projeto

---
+++ PP1.CONTRATO/PP1.CONTRATO.WEB/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace PP1.CONTRATO.WEB
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ PP1.CONTRATO/PP1.CONTRATO.WEB/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PP1.CONTRATO.WEB.MvcApplication" Language="C#" %>
