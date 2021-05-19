24e8b0d426085209adb660bbee75a89d42a908a8
Regis Abegg <regisabegg@gmail.com>
2021-05-18 22:28:03 -0300

    Telas de Cadastro

---
+++ PP1.CONTRATO/PP1.CONTRATO.WEB/Models/Pais/PaisVM.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PP1.CONTRATO.WEB.Models.Pais
+{
+    public class PaisVM
+    {
+        [Display(Name = "Código")]
+        public int idPais { get; set; }
+        [Display(Name = "Nome")]
+        [Required]
+        public string nmPais { get; set; }
+        [Display(Name = "Sigla")]
+        public string dsSigla { get; set; }
+        [Display(Name = "DDI")]
+        public string nrDDI { get; set; }
+        [Display(Name = "Cadastro")]
+        [DisplayFormat(DataFormatString = "mm/dd/yyyy")]
+        public DateTime dtCadastro { get; set; }
+        [Display(Name = "Atualização")]
+        [DisplayFormat(DataFormatString = "mm/dd/yyyy")]
+        public DateTime dtAtualizacao { get; set; }
+    }
+}
